bbc12a9c312895c000d9a240386cd5c534f6a5a4
a LeNdA <you@example.com>
2019-03-20 11:45:50 -0300

    bla

--- py-flask.py
+++ py-flask.py
@@ -1,6 +1,13 @@
 from flask import Flask,render_template
 app= Flask(__name__)
 
+class Pessoa():
+    def __init__(self,nome,idade,nascimento):
+        self.nome=nome
+        self.idade=idade
+        self.nascimento=nascimento
+
+
 @app.route("/")
 def carai():
     return render_template("inicio.html")
@@ -11,7 +18,8 @@ def caramba():
 
 @app.route("/listapessoa")
 def caramb():
-    return render_template("listarpessoa.html")
+    lista=[Pessoa("Piske",17,"12/09/2001"), Pessoa("Ravi",17,"16/07/2001")]
+    return render_template("listarpessoa.html", So_cara_foda= lista)
 
 
 app.run(debug=True)
