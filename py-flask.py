67c2b1d3ae3eec8294143c44805c39b535f72ead
a LeNdA <you@example.com>
2019-03-27 11:43:30 -0300

    bla

--- py-flask.py
+++ py-flask.py
@@ -17,15 +17,31 @@ def carai():
 def caramba():
     return render_template("adicionarpessoa.html")
 
+@app.route("/listapessoa_sem_add")
+def caramb1():
+
+    return render_template("listarpessoa.html", So_cara_foda= lista)
+
 @app.route("/listapessoa")
-def caramb():
+def caramb2():
 
     nome= request.args.get("Nome")
     idade= request.args.get("Idade")
     nasci= request.args.get("Nascimento")
-    lista=[Pessoa("Piske",17,"12/09/2001"), Pessoa("Ravi",17,"16/07/2001")]
     lista.append(Pessoa(nome,int(idade),nasci))
-    return render_template("listarpessoa.html", So_cara_foda= lista)
+
+    return caramb1()
+
+@app.route("/deletepessoa")
+def caramb3():
+
+    nome= request.args.get("Nome")
+    for pessoa in lista:
+        if nome== pessoa.nome:
+            lista.remove(pessoa)
+            break
+    return render_template("mensagem.html")
 
 
 app.run(debug=True)
+
