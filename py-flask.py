34b00e5a1e9f0753220347a8801d49670ae303bb
a LeNdA <you@example.com>
2019-03-28 09:08:14 -0300

    bla

--- py-flask.py
+++ py-flask.py
@@ -2,12 +2,13 @@ from flask import Flask,render_template,request
 app= Flask(__name__)
 
 class Pessoa():
-    def __init__(self,nome,idade,nascimento):
+    def __init__(self,nome,idade,nascimento,cpf):
         self.nome=nome
         self.idade=idade
         self.nascimento=nascimento
+        self.cpf=cpf
 
-lista=[Pessoa("Piske",17,"12/09/2001"), Pessoa("Ravi",17,"16/07/2001")]
+lista=[Pessoa("Piske",17,"12/09/2001","123.124.213-20"), Pessoa("Ravi",17,"16/07/2001","432.234.987.01")]
 
 @app.route("/")
 def carai():
@@ -28,16 +29,24 @@ def caramb2():
     nome= request.args.get("Nome")
     idade= request.args.get("Idade")
     nasci= request.args.get("Nascimento")
-    lista.append(Pessoa(nome,int(idade),nasci))
-
+    cpf= request.args.get("Cpf")
+    val=0
+    # comparar cpfs
+    while val==0:
+        for pessoa in lista:
+            if cpf == pessoa.cpf:
+                return render_template("erro_add_pessoa.html")
+        val=1
+        
+    lista.append(Pessoa(nome,int(idade),nasci,cpf))
     return caramb1()
 
 @app.route("/deletepessoa")
 def caramb3():
 
-    nome= request.args.get("Nome")
+    cpf= request.args.get("Cpf")
     for pessoa in lista:
-        if nome== pessoa.nome:
+        if cpf== pessoa.cpf:
             lista.remove(pessoa)
             break
     return render_template("mensagem.html")
