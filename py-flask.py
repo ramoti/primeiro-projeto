bcf4a56541405a4d964d7c6021292d358a97c5fd
”André <andre.heydran@gmail.com>
2019-04-25 13:30:49 -0300

    bla

--- py-flask.py
+++ py-flask.py
@@ -10,45 +10,72 @@ class Pessoa():
 
 lista=[Pessoa("Piske",17,"12/09/2001","123.124.213-20"), Pessoa("Ravi",17,"16/07/2001","432.234.987.01")]
 
+
+
 @app.route("/")
 def carai():
     return render_template("inicio.html")
+
+
+
 
 @app.route("/login_form")
 def cadas():
     return render_template("login.html")
 
-@app.route("/login")
+
+
+
+
+@app.route("/login", methods=["POST"])
 def cadas2():
-    nome=request.args.get("Nome")
-    senha=request.args.get("Senha")
+    nome=request.form["Nome"]
+    senha=request.form["Senha"]
     if nome=="Piske" and senha=="123":
         session["usuario"]=nome
         return redirect("/")
     else:
         return "sua senha ou/e nome podem estar ERRADOS"
 
+
+
+
+
 @app.route("/logout")
 def logout () :  
     session.pop("usuario") 
     return redirect("/")
 
+
+
+
+
+
 @app.route("/addpessoa")
 def caramba():
     return render_template("adicionarpessoa.html")
+
+
+
+
 
 @app.route("/listapessoa_sem_add")
 def caramb1():
 
     return render_template("listarpessoa.html", So_cara_foda= lista)
 
-@app.route("/listapessoa")
+
+
+
+
+
+@app.route("/listapessoa", methods=["POST"])
 def caramb2():
 
-    nome= request.args.get("Nome")
-    idade= request.args.get("Idade")
-    nasci= request.args.get("Nascimento")
-    cpf= request.args.get("Cpf")
+    nome= request.form["Nome"]
+    idade= request.form["Idade"]
+    nasci= request.form["Nascimento"]
+    cpf= request.form["Cpf"]
     val=0
     # comparar cpfs
     while val==0:
@@ -58,7 +85,11 @@ def caramb2():
         val=1
         
     lista.append(Pessoa(nome,int(idade),nasci,cpf))
-    return caramb1()
+    return redirect( "/listapessoa_sem_add" )
+
+
+
+
 
 @app.route("/deletepessoa")
 def caramb3():
@@ -71,6 +102,9 @@ def caramb3():
     return render_template("mensagem.html")
 
 
+
+
+
 @app.route("/form_alterar")
 def caramb4():
 
@@ -79,13 +113,18 @@ def caramb4():
         if cpf== pessoa.cpf:
             return render_template("form_alterar.html", pessoa=pessoa)
 
-@app.route("/alterar_pessoa")
+
+
+
+
+
+@app.route("/alterar_pessoa", methods=["POST"])
 def caramb5():
 
-    nome= request.args.get("Nome")
-    idade= request.args.get("Idade")
-    nasci= request.args.get("Nascimento")
-    cpf= request.args.get("Cpf")
+    nome= request.form["Nome"]
+    idade= request.form["Idade"]
+    nasci= request.form["Nascimento"]
+    cpf= request.form["Cpf"]
     pessoa_alterada= Pessoa(nome,idade,nasci,cpf)
     for pessoa in range(len(lista)):
         if cpf== lista[pessoa].cpf:
