8723083e780bc46e8f51ebc4ec0d6c721167cf05
christopher <christopherpiske@gmail.com>
2019-06-03 14:19:28 -0300

    bla

--- mercado.py
+++ mercado.py
@@ -1,6 +1,6 @@
 import peewee, os
 
-db = peewee.SqliteDatabase ( ’ animalia.db’)
+db = peewee.SqliteDatabase ("animalia.db")
 
 class Animal(peewee.Model):
 
@@ -12,7 +12,7 @@ class Animal(peewee.Model):
         database = db
 
     def str ( self ) :
-        return self . tipo animal+”,”+ self . raca+” de ”+ self .nomedono
+        return self.tipo animal+","+ self.raca+" de "+ self.nomedono
 
 class Consulta(peewee.Model):
     data = peewee.CharField ()
@@ -26,7 +26,7 @@ class Consulta(peewee.Model):
     database = db
 
     def str ( self ) :
-        return self . servico +" em "+ self .data+": "+ self . horario+", confirmado: "+ "\n" self . confirma+”, ID da consulta : ”+ self .myID+” | animal: ”+str( self .animal)
+        return self.servico +" em "+ self.data+": "+ self.horario+", confirmado: "+ "\n" self.confirma+", ID da consulta : "+ self.myID+" | animal: "+str(self.animal)
 
 
 
@@ -35,9 +35,4 @@ class Consulta(peewee.Model):
 
 
 if __name__ == "__main__":
-    produto1= Produto("carne", 0.75)
-    produto2= Produto("batata doce", 3.50)
-    item= Item(produto1, 2)
-    item2= Item(produto2, 1.750)
-    carrinho= Carrinho([item,item2])
-    carrinho.mostra_carrinho()
+    
